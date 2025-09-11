4953b6fe0bc1f8e2114ddf16015789fd8c1826f4
Bunz029 <vinceerrol.l.abrera@isu.edu.ph>
2025-09-11 21:10:00 +0800

    chore(cors): allow isuetest.netlify.app and netlify subdomains

--- config/cors.php
+++ config/cors.php
@@ -3,8 +3,15 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:8081', 'https://isueadmin.netlify.app'],
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => [
+        'http://localhost:8080',
+        'http://localhost:3000',
+        'http://localhost:8081',
+        'https://isueadmin.netlify.app',
+        'https://isuetest.netlify.app',
+    ],
+    // Allow any Netlify subdomain (optional, safer to list exact domains above)
+    'allowed_origins_patterns' => ['#^https:\/\/[a-z0-9-]+\.netlify\.app$#i'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
